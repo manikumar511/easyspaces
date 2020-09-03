cc6f170f2b85bf1773ed8fab3c3c8b6e45772f05
manikumar511 <64631719+manikumar511@users.noreply.github.com>
2020-09-03 23:14:08 +0530

    Commit manis changes

---
+++ force-app/main/default/lwc/accsearch/accsearch.js
@@ -0,0 +1,10 @@
+import { LightningElement,track } from 'lwc';
+
+export default class Accsearch extends LightningElement {
+    @track name1
+    name='manikumar';
+    changename(event){
+        console.log('event.target.value======='+event.target.value);
+        name1=event.target.value;
+    }
+}
