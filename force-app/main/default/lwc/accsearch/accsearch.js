c7554c5a2dcb5e01e3280ba09305c9b67f324d89
manikumar511 <64631719+manikumar511@users.noreply.github.com>
2020-09-03 23:27:45 +0530

    var value reverted back

--- force-app/main/default/lwc/accsearch/accsearch.js
+++ force-app/main/default/lwc/accsearch/accsearch.js
@@ -2,7 +2,7 @@ import { LightningElement,track } from 'lwc';
 
 export default class Accsearch extends LightningElement {
     @track name1
-    name='manikumar gunda';
+    name='manikumar';
     changename(event){
         console.log('event.target.value======='+event.target.value);
         name1=event.target.value;
