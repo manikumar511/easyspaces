d691454a2c5eede0653e9d9b135d568b605b8938
ManiKumar <manikumarcrm@gmail.com>
2022-12-24 23:26:18 -0500

    Commit two

--- force-app/main/default/lwc/accsearch/accsearch.js
+++ force-app/main/default/lwc/accsearch/accsearch.js
@@ -2,7 +2,7 @@ import { LightningElement,track } from 'lwc';
 
 export default class Accsearch extends LightningElement {
     @track name1
-    name='actor rajinikanth';
+    name='actor rajinikanth abc';
     changename(event){
         console.log('event.target.value======='+event.target.value);
         name1=event.target.value;
